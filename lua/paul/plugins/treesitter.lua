b3ef322765fb2ba33083aae9f935fb863a53b65d
Paul Creegan <creeganpaul9@gmail.com>
2025-02-17 15:47:43 -0500

    removes lsp and added treesitter again

--- lua/paul/plugins/treesitter.lua
+++ lua/paul/plugins/treesitter.lua
@@ -1,14 +1,14 @@
-return ({{
+return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function () 
       local configs = require("nvim-treesitter.configs")
 
       configs.setup({
-          ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+          ensure_installed = { "c", "lua", "vim", "vimdoc", "html", "java", "rust", "css" },
           sync_install = false,
           highlight = { enable = true },
           indent = { enable = true },  
         })
     end
- }})
+ }
