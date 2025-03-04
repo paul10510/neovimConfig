34037e58271cc79817178e26fb14b7964e68a5e4
Paul Francis Creegan <pfc8737@rit.edu>
2025-03-04 11:01:09 -0500

    added keymap to split a terminal window

--- lua/paul/core/keymaps.lua
+++ lua/paul/core/keymaps.lua
@@ -14,6 +14,7 @@ keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decremen
 -- window management
 keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
 keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
+keymap.set("n", "<leader>st", ":10sp<CR>:terminal<CR>", { desc = "split a terminal window" })
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
 
